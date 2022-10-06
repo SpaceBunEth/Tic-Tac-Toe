74c7dbc9a4d3beda3b3484e49f762619c653a836
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-10-06 20:12:02 +0000

    ReUsed code from weather App to help create html elements with js in DOM

--- js/sandbox-bootstrap.js
+++ js/sandbox-bootstrap.js
@@ -1,1 +1,46 @@
 console.log('sandbox-bootstrap')
+
+
+
+//-----------Start of ReUsed Code-----------
+
+// Set const to body and assign id with "bodyid"
+let htmlBody = document.querySelector('body');
+htmlBody.setAttribute('id','bodyid');
+
+// Create const mainDiv holds all html elements for weather app
+const mainDiv = document.createElement('div');
+mainDiv.setAttribute('id','main');
+htmlBody.appendChild(mainDiv);
+
+// Create a set of headerDiv and bodyDiv for our content
+// Use as reference for when appending childs
+const headerDiv = document.createElement('div');
+headerDiv.setAttribute('id', 'headerDiv');
+htmlBody.appendChild(headerDiv);
+const bodyDiv = document.createElement('div');
+bodyDiv.setAttribute('id', 'bodyDiv');
+htmlBody.appendChild(bodyDiv);
+
+
+// testing append on body
+// htmlBody.appendChild(mainDiv)
+// htmlBody.appendChild(titleTextElem)
+
+// Function for creating html elements for DOM with JS
+// Create a func with Params passing
+// html element name and name of the id
+// then append element to a parent Param passed in the function
+function createElem(htmlElem, idName, parentElem, className = null){
+    const parent = document.getElementById(parentElem)
+    const makeElem = document.createElement(htmlElem);
+    makeElem.setAttribute('id', idName); 
+    if (className !== null){
+        makeElem.setAttribute('class', className)
+        parent.appendChild(makeElem);
+    }
+}
+//-------------End Of Re-Used Code------
+
+
+
